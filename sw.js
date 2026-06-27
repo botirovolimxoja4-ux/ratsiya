c1d93cd9999c59dab08874dc1acb02441a49e8a1
botirovolimxoja4-ux <botirovolimxoja4@gmail.com>
2026-06-27 20:23:45 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ratsiya-v1";
+const CACHE = "ratsiya-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
